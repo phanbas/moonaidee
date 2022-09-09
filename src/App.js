0e6651bc03c62542e230e147ecdcd37a9ec5837e
phanbas <phan.bas.work@gmail.com>
2022-09-09 10:56:04 +0700

    Node JS TODO File first time

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect } from 'react';
+import Item from './components/Item';
+import Transaction from './components/Transaction';
+import {useState} from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+          <div>
+          <h1>Phan Love Pratoo 4Ever</h1>
+          </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Transaction.js
@@ -0,0 +1,14 @@
+import Item from "./Item";
+
+
+const Transaction = ()=> {
+    return (
+      <ul>
+          <Item/>
+          <Item/>
+          <Item/>
+      </ul>
+    );
+  }
+
+export default Transaction;
